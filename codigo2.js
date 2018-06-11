1342c6fa07338e98a57e4df24099e212c4d7c692
Zacarías <zacariassamaniego@gmail.com>
2018-06-11 00:35:42 -0300

    First commit

---
+++ codigo2.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    var cuadrado = '<div class="cuadrado"></div>';
+    
+    
+    $('#agregarHijo').click(function(){
+        $('.contenedor').append(cuadrado);
+    });
+    $('#eliminarHijo').click(function(){});
+
+
+
+
+    // Select the node that will be observed for mutations
+var targetNode = document.getElementById('some-id');
+
+// Options for the observer (which mutations to observe)
+var config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+var callback = function(mutationsList) {
+    for(var mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+            console.log($('.contenedor').children.length);
+            console.log('A child node has been added or removed.');
+        }
+        // else if (mutation.type == 'attributes') {
+        //     console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        // }
+    }
+};
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+// Later, you can stop observing
+// observer.disconnect();
+});
